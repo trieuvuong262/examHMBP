b2538150082458576afd0f7f5afda9a79c36b4d5
trieuvuong262 <trieuvuong1997@gmail.com>
2026-06-10 09:10:32 +0700

    update

--- hrm/user_search.py
+++ hrm/user_search.py
@@ -100,15 +100,15 @@ USER_LIST_SORT_COLUMNS = {
 
 USER_LIST_TABLE_COLUMNS = (
     {'key': 'code', 'label': 'Mã NS', 'th_tone': 'cap', 'th_align': 'start', 'col_class': 'jp-hrm-col-code', 'sortable': True},
-    {'key': 'name', 'label': 'Họ và tên', 'th_tone': 'proper', 'th_align': 'start', 'col_class': 'jp-hrm-col-name', 'sortable': True},
+    {'key': 'name', 'label': 'Họ và tên', 'th_tone': 'cap', 'th_align': 'start', 'col_class': 'jp-hrm-col-name', 'sortable': True},
     {'key': 'account', 'label': 'Account', 'th_tone': 'cap', 'th_align': 'start', 'col_class': 'jp-hrm-col-account', 'sortable': True},
-    {'key': 'department', 'label': 'Phòng ban', 'th_tone': 'proper', 'th_align': 'start', 'col_class': 'jp-hrm-col-org', 'sortable': True},
-    {'key': 'division', 'label': 'Bộ phận', 'th_tone': 'proper', 'th_align': 'start', 'col_class': 'jp-hrm-col-org', 'sortable': True},
-    {'key': 'position', 'label': 'Vị trí', 'th_tone': 'proper', 'th_align': 'start', 'col_class': 'jp-hrm-col-org', 'sortable': True},
-    {'key': 'job_title', 'label': 'Chức vụ', 'th_tone': 'proper', 'th_align': 'start', 'col_class': 'jp-hrm-col-job-title', 'sortable': True},
+    {'key': 'department', 'label': 'Phòng ban', 'th_tone': 'cap', 'th_align': 'start', 'col_class': 'jp-hrm-col-dept', 'sortable': True},
+    {'key': 'division', 'label': 'Bộ phận', 'th_tone': 'cap', 'th_align': 'start', 'col_class': 'jp-hrm-col-division', 'sortable': True},
+    {'key': 'position', 'label': 'Vị trí', 'th_tone': 'cap', 'th_align': 'start', 'col_class': 'jp-hrm-col-position', 'sortable': True},
+    {'key': 'job_title', 'label': 'Chức vụ', 'th_tone': 'cap', 'th_align': 'start', 'col_class': 'jp-hrm-col-job-title', 'sortable': True},
     {'key': 'birth_date', 'label': 'Ngày sinh', 'th_tone': 'cap', 'th_align': 'center', 'col_class': 'jp-hrm-col-date', 'sortable': True},
     {'key': 'gender', 'label': 'Giới tính', 'th_tone': 'cap', 'th_align': 'center', 'col_class': 'jp-hrm-col-gender', 'sortable': True},
-    {'key': 'permission_group', 'label': 'Nhóm quyền', 'th_tone': 'proper', 'th_align': 'start', 'col_class': 'jp-hrm-col-perm-group', 'sortable': True},
+    {'key': 'permission_group', 'label': 'Nhóm quyền', 'th_tone': 'cap', 'th_align': 'start', 'col_class': 'jp-hrm-col-perm-group', 'sortable': True},
     {'key': 'role', 'label': 'Vai trò HT', 'th_tone': 'cap', 'th_align': 'center', 'col_class': 'jp-hrm-col-role', 'sortable': True},
     {'key': None, 'label': 'Thao tác', 'th_tone': 'cap', 'th_align': 'end', 'col_class': 'jp-hrm-col-actions', 'sortable': False},
 )
