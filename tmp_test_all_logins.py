00b8f4ed48b8a246effa6c962e787bd66bb819fd
trieuvuong262 <trieuvuong1997@gmail.com>
2026-06-26 11:52:53 +0700

    update

--- tmp_test_all_logins.py
+++
@@ -1,8 +0,0 @@
-import os, django
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'PortalJustPlay.settings')
-django.setup()
-from django.contrib.auth import authenticate
-import xmlrpc.client
-print('Portal auth:', bool(authenticate(username='test-ldap', password='TestLdap@123')))
-c = xmlrpc.client.ServerProxy('http://odoo-web:8069/xmlrpc/2/common', allow_none=True)
-print('Odoo uid:', c.authenticate('justplay_pilot', 'test-ldap', 'TestLdap@123', {}))
